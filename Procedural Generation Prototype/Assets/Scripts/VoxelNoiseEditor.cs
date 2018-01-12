8e5baf7f4660d464ee4592d88a24d49fe447ea84
Muff1nz <muffinz314@gmail.com>
2018-01-12 06:10:59 +0100

    Added 1D simplex noise, and editor buttons for creating terrain

---
+++ Procedural Generation Prototype/Assets/Scripts/VoxelNoiseEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VoxelNoiseTerrain))]
+public class VoxelNoiseEditor : Editor {
+
+    VoxelNoiseTerrain VNT;
+
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        VNT = (VoxelNoiseTerrain)target;
+
+        if (GUILayout.Button("1D Simplex")) {
+            VNT.gen1DSimplex();
+        }
+
+        if (GUILayout.Button("Kill all cubes")) {
+            VNT.killAllCubes();
+        }
+    }
+}
---
+++ Procedural Generation Prototype/Assets/Scripts/VoxelNoiseTerrain.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VoxelNoiseTerrain))]
+public class VoxelNoiseTerrain : MonoBehaviour {
+
+    private List<GameObject> voxels = new List<GameObject>();
+
+    public void killAllCubes() {
+        clearVoxels();
+        MeshFilter[] cubes = GameObject.FindObjectsOfType<MeshFilter>();
+        foreach (var cube in cubes) DestroyImmediate(cube.gameObject);
+    }
+
+    public void gen1DSimplex() {
+        StartCoroutine(generateWorld1DSimplex(20));
+    }
+	
+    private void clearVoxels() {
+        foreach (var voxel in voxels) DestroyImmediate(voxel);
+        voxels.Clear();
+    }
+
+	private IEnumerator generateWorld1DSimplex(int size) {
+        clearVoxels();
+        const int speed = 200;
+        int count = 0;
+       
+        for (int x = -size; x <= size; x++) {
+
+            if (count++ > speed) {
+                count = 0;
+                yield return 0;
+            }
+
+            
+            var voxel = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            voxels.Add(voxel);
+            voxel.transform.position = new Vector3(x, SimplexNoise.SimplexValue1D(new Vector3(x, 0), 1.5f));
+            
+        }
+    }
+}
